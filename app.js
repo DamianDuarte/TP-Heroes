f1a0e82106f820642a711a2f39efe372b2327779
DamianDuarte <106710013+DamianDuarte@users.noreply.github.com>
2022-06-23 15:32:01 -0300

    agrego contenido a index

--- app.js
+++ app.js
@@ -1,9 +1,13 @@
 const express = require('express');
+const { dirname } = require('path');
 const app = express();
 const path = require('path');
+const port = 3030;
 
-app.use(express.static('public'));
-app.get("/", (req, res) => res.sendFile(path.join(__dirname, 'views', 'index.html')));
+/* app.use(express.static('public')); */
+
+
+app.get("/", (req, res) =>res.sendFile(path.join(__dirname, 'views', 'index.html')))
 app.get("/babbage", (req, res) => res.sendFile(path.join(__dirname, 'views', 'babbage.html')));
 app.get("/berners-lee", (req, res) => res.sendFile(path.join(__dirname, 'views', 'berners-lee.html')));
 app.get("/clarke", (req, res) => res.sendFile(path.join(__dirname, 'views', 'clarke.html')));
@@ -12,4 +16,7 @@ app.get("/hopper", (req, res) => res.sendFile(path.join(__dirname, 'views', 'hop
 app.get("/lovelace", (req, res) => res.sendFile(path.join(__dirname, 'views', 'lovelace.html')));
 app.get("/turing", (req, res) => res.sendFile(path.join(__dirname, 'views', 'turing.html')));
 
-app.listen(3030, () => console.log("Server running in port 3030"))
+app.listen(port, () =>
+{
+    console.log('Server is running in http://localhost:' + port);
+});
