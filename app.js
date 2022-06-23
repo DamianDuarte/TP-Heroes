208fa3af35645c89488f51a76870779bbbf35abe
DamianDuarte <106710013+DamianDuarte@users.noreply.github.com>
2022-06-23 15:36:14 -0300

    quito views de dentro de public

--- app.js
+++ app.js
@@ -4,10 +4,10 @@ const app = express();
 const path = require('path');
 const port = 3030;
 
-/* app.use(express.static('public')); */
+app.use(express.static('public'));
 
 
-app.get("/", (req, res) =>res.sendFile(path.join(__dirname, 'views', 'index.html')))
+app.get("/", (req, res) =>res.sendFile(path.join(__dirname,'views', 'index.html')))
 app.get("/babbage", (req, res) => res.sendFile(path.join(__dirname, 'views', 'babbage.html')));
 app.get("/berners-lee", (req, res) => res.sendFile(path.join(__dirname, 'views', 'berners-lee.html')));
 app.get("/clarke", (req, res) => res.sendFile(path.join(__dirname, 'views', 'clarke.html')));
